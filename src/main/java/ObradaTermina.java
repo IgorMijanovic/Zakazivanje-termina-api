d3e5b85b6b02b1bb859ca6039a584320c724d36b
IgorM213 <igor.mijanovic02@gmail.com>
2023-11-12 01:22:27 +0100

    bb

--- src/main/java/ObradaTermina.java
+++ src/main/java/ObradaTermina.java
@@ -7,13 +7,13 @@ public abstract class ObradaTermina {
     private List<Prostor> prostori;
 
 
-    public abstract boolean dodajNoviTermin();
-    public abstract boolean brisanjeTermina();
-    public abstract boolean premestanjeTermina();
+    public abstract boolean dodajNoviTermin(String... args);
+    public abstract boolean brisanjeTermina(String... args);
+    public abstract boolean premestanjeTermina(String... args);
 
-    public abstract boolean dodavanjeProstorija();
+    public abstract boolean dodavanjeProstorija(String... args);
 
-    public boolean pretragaTermina(){
+    public boolean pretragaTermina(String... args){
 
         return true;
     }
