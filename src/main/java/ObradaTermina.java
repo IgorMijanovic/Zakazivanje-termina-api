9549f196a523dc5dcff494b0cff9c812575ba8a6
AleksaNikolic102 <anikolic10222rn@raf.rs>
2023-11-12 17:42:15 +0100

    aki

--- src/main/java/ObradaTermina.java
+++ src/main/java/ObradaTermina.java
@@ -1,12 +1,15 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class ObradaTermina {
 
     private List<Termin> raspored;
     private List<Prostor> prostori;
 
-    public abstract boolean dodajNoviTermin();
+    public abstract boolean dodajNoviTermin(Prostor prostor, LocalDateTime pocetak, LocalDateTime kraj, Map<String,String> dodaci);
     public abstract boolean brisanjeTermina();
     public abstract boolean premestanjeTermina();
     public abstract boolean pretragaTermina();
